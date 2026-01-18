1fcb0791f1b33d5b376ac58a0b5d003745e8648f
Aryan Shrivastava <aryan1shrivastava@gmail.com>
2026-01-18 17:33:53 +0530

    feat: implemented keyword intersection logic

--- src/main/java/com/ResumeMatch/resume_matcher/controller/MatchingController.java
+++ src/main/java/com/ResumeMatch/resume_matcher/controller/MatchingController.java
@@ -1,10 +1,13 @@
 package com.ResumeMatch.resume_matcher.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.*;
+import com.ResumeMatch.resume_matcher.dto.MatchRequest;
+import com.ResumeMatch.resume_matcher.dto.MatchResponse;
 
 @RestController
 @RequestMapping("/api")
@@ -12,6 +15,40 @@ public class MatchingController {
     @GetMapping("/health")
     public String health(){
         return "Resume Matcher is up and running 🚀";
+    }
+
+    @PostMapping("/match")
+    public ResponseEntity<MatchResponse> matchResume(@RequestBody MatchRequest request){
+
+        String resume = request.getResumeText();
+        String jobDescription = request.getJobDescription();
+
+        double actualScore = calculateMatchScore(resume, jobDescription);
+
+        return ResponseEntity.ok(new MatchResponse(actualScore));
+    }
+
+    private double calculateMatchScore(String resumeText, String jdText){
+
+        if(resumeText == null || jdText == null || jdText.isEmpty()){
+            return 0.0;
+        }
+
+        String[] resumeWords = resumeText.toLowerCase().split("\\W+");
+        String[] jdWords = jdText.toLowerCase().split("\\W+");
+
+        java.util.Set<String> resumeWordSet = new java.util.HashSet<>(java.util.Arrays.asList(resumeWords));
+
+        long matchCount = java.util.Arrays.stream(jdWords)
+                .distinct() // Don't count "Java" twice if it's in the JD twice
+                .filter(word -> resumeWordSet.contains(word))
+                .count();
+
+        long totalUniqueJdWords = java.util.Arrays.stream(jdWords).distinct().count();
+
+        if (totalUniqueJdWords == 0) return 0.0;
+        return ((double) matchCount / totalUniqueJdWords) * 100;
+
     }
 }
 
--- src/main/java/com/ResumeMatch/resume_matcher/dto/MatchResponse.java
+++ src/main/java/com/ResumeMatch/resume_matcher/dto/MatchResponse.java
@@ -1,4 +1,17 @@
 package com.ResumeMatch.resume_matcher.dto;
 
 public class MatchResponse {
+    private double matchPercentage;
+
+    public MatchResponse( double matchPercentage ) {{
+    this.matchPercentage = matchPercentage;}
+    }
+
+    public double getMatchPercentage(){
+        return matchPercentage;
+    }
+
+    public void setMatchPercentage( double matchPercentage ){
+        this.matchPercentage = matchPercentage;
+    }
 }
